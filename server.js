af60cf94fa6b1c061957a0476f5e00dd93ecd199
labadcloyd <abadcloyd@gmail.com>
2021-06-11 01:10:19 +0000

    set up socket io

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const next = require("next");
@@ -8,6 +7,13 @@ const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 const port = process.env.PORT || 3000
 require('dotenv').config({path:'./config.env'})
+
+io.on('connection', (socket)=>{
+  console.log('connected')
+  socket.on('welcome', (arg)=>{
+    console.log(arg)
+  })
+})
 
 nextApp.prepare().then(() => {
   
