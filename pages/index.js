e3bb454d907dd231b7c2bde3cd8493449a82a448
labadcloyd <abadcloyd@gmail.com>
2021-06-12 01:10:19 +0000

    setting up html structure of homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import Head from 'next/head'
-import socketio from 'socket.io-client'
+
 
 export default function Home() {
-  const io = socketio()
-  io.on('connect', (data)=>{
-    console.log('connected to socket')
-    io.emit('welcome', {message:"Hello world"}) 
-  })
+ 
+  
   return (
-    <div >
+    <div>
+      {/* search bar */}
+      <div>
+        <form>
+          <input type="search"></input>
+        </form>
+      </div>
+      <div>
+        {/* messages div */}
+        <div></div>
+        {/* message bar */}
+        <form></form>
+      </div>
+      {/* username prompt */}
+      <div style={{display:showPrompt?'flex':'none', justifyContent:'center', alignItems:'center', position:'fixed',top:'0', left:'0', width:'100%', height:'100%', backgroundColor:'#0000005c'}}>
+        <form onSubmit={changeUsername}>
+          <input value={usernameInput} onChange={(e)=>{setUsernameInput(e.target.value)}}></input>
+          <button type='submit'></button>
+        </form>
+      </div>
     </div>
   )
 }
